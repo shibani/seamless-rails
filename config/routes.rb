246bba544f463dd1d88d58487cf6efe2adb71e19
Shibani Mookerjee <shibani@kindreddevelopment.com>
2016-01-23 23:11:52 -0500

    adding site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'site/index'
+
+  get 'site/show'
+
+  get 'site/create'
+
+  root to: 'site#index'
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
