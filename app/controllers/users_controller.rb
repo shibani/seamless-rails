ed95ccaa1301f472d5f24b83dc49f1fd217e74bc
Shibani <shibani@kindreddevelopment.com>
2016-06-19 14:17:48 -0400

    fixing params again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,13 @@ class UsersController < ApplicationController
             #render '/users/remote_create.json.erb', :locals =>  { restaurants: @restaurants}
             render '/users/create.json.erb'
           end
+        elsif params[:form] == "remote_signup"
+          if @user.save
+            @notice = "Sign up was successful!"
+            #render "remote_create"
+            #render '/users/remote_create.json.erb', :locals =>  { restaurants: @restaurants}
+            render '/users/create.json.erb'
+          end
         end
       else
         @error = true
@@ -32,6 +39,24 @@ class UsersController < ApplicationController
         if signup_params[:form] == "signup"
           render "new"
         elsif signup_params[:form] == "remote_signup"
+          @notice = "Sign up failed"
+          
+          #Rails.logger.debug "check: " + @user.errors.full_messages.inspect
+
+          @user.errors.messages[:email] == ["is invalid"] ? @email_error1 = true : @email_error1 = false
+          @user.errors.messages[:email] == ["already has an account associated with it"] ? @email_error2 = true : @email_error2 = false
+          @user.errors.messages[:name] == ["is too short (minimum is 4 characters)"] ? @name_error1 = true : @name_error1 = false
+          @user.errors.messages[:name] == ["is too long (maximum is 50 characters)"] ? @name_error2 = true : @name_error2 = false
+          @user.errors.messages[:password] == ["can't be blank"] ? @pwd_error1 = true : @pwd_error1 = false
+          @user.errors.messages[:password] == ["must be 8 characters or longer"] ? @pwd_error2 = true : @pwd_error2 = false
+
+          #Rails.logger.debug "check: " + @user.errors.messages[:email].inspect
+
+          Rails.logger.debug "check: " + @email_error1.inspect
+
+          render '/users/create.json.erb'
+          
+        elsif params[:form] == "remote_signup"
           @notice = "Sign up failed"
           
           #Rails.logger.debug "check: " + @user.errors.full_messages.inspect
