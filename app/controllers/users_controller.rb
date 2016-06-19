b862567bda9e3be45c8867c095011e66ab8afc7d
Shibani <shibani@kindreddevelopment.com>
2016-06-19 14:10:53 -0400

    removing skip authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token, :if =>lambda{ signup_params[:form] == "remote_signup" }
+  #skip_before_filter :verify_authenticity_token, :if =>lambda{ signup_params[:form] == "remote_signup" }
 
   def new
     @bodyclass = "signup"
