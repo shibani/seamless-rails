cf90eda538317d69625891a31997704234e08bbd
Shibani <shibani@kindreddevelopment.com>
2016-08-15 19:53:05 -0400

    updating mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_mailer.default_url_options = { host: 'us-cdbr-iron-east-04.cleardb.net'}
+  config.action_mailer.default_url_options = { host: 'us-cdbr-iron-east-04.cleardb.net'}
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'sm-seamless.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
